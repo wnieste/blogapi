af9119254d16761dd5f8739b7c4b181a017cda65
W. Niestempski <wnieste@orthnac.apnnetworks.com>
2018-02-01 20:08:14 -0800

    cleaned up the code, working at this time.

--- modules/blogpost.js
+++ modules/blogpost.js
@@ -1,5 +1,7 @@
-function handle_GET_request( request, response, dbHandle )  {
-  //  the /posts endpoint must use the GET method
+const debug = false ;
+
+function return_all_posts( request, response, dbHandle )  {
+  //  this endpoint must use the GET method, otherwise we abort
   if (request.method != 'GET' )
   {
     response.writeHead( 405, {
@@ -9,26 +11,35 @@ function handle_GET_request( request, response, dbHandle )  {
   }
   else
   {
+    //  as we've been given a database with an existing schema, we shall hardcode this query.
     let sqlQuery = 'SELECT post_id as ID,  title as TITLE, body as POST from posts' ;
 
     response.writeHead( 200,  {
       'Content-Type' : 'application/json'
     } ) ;
+    //  returning an array of blogposts
     response.write( '{ "blogposts": ' ) ;
+    
     // loop through blog posts
+    //  NOTE:  works for our example but will crash with a large number of db entries as
+    //    the call to dbHandle.all will load ALL posts in memory
     dbHandle.all( sqlQuery, [], (err, row ) =>  {
       if (err) {
         throw err ;
       }
+        //  will create an array of JSON objects as a string of characters
         let blogPost = JSON.stringify( row ) ;
-        console.log( blogPost ) ;
         response.write( blogPost, function( error ) { response.end( ' } ' ) } ) ;
+        
+        if (debug)  {
+          console.log( blogPost ) ;			//  debug
+        }
     } ) ;
   }
 }
 
-function handle_POST_request( request, response, dbHandle )  {
-  //  the /post endpoint must use the POST method
+function add_blog_post( request, response, dbHandle )  {
+  //  the /post endpoint must use the POST method, otherwise we abort
   if (request.method != 'POST' )
   {
     response.writeHead( 405, {
@@ -38,21 +49,37 @@ function handle_POST_request( request, response, dbHandle )  {
   }
   else
   {
-    let dbInsert = 'INSERT INTO posts (title, body) VALUES ( "foo", "bar" )' ;
-    dbHandle.run( dbInsert, [], (err) => {
-      if (err)  {
-        return console.error( err.message ) ;
-      }
-      console.log ('inserted foo-bar' ) ;
+    request.on( 'data', function (data) {
+      //  need to get the POST'ed data and convert into distinct fields
+      let blogPOST = JSON.parse( data ) ;
+
+      //  build our INSERT statement from the POST'ed data
+      let dbInsert = 'INSERT INTO posts (title, body) VALUES ( "' 
+          + `${blogPOST.title}` + '", "' + `${blogPOST.body}` + '" )' ;
+
+      //  INSERT a new blog post into the datastore
+      dbHandle.run( dbInsert, [], (err) => {
+        if (err)  {
+          return console.error( err.message ) ;
+        }
+
+        if (debug)  {
+          console.log ('inserted foo-bar' ) ;		//  debug
+        }
+
+      } ) ;
     } ) ;
+
     response.writeHead( 200,
       {
         'Content-Type' : 'text/plain'
       }
     ) ;
-    response.end( 'POST action executed' ) ;
+    //response.end( 'POST action executed' ) ;
+    response.end() ;
   }
 }
 
-exports.fetchEntries = handle_GET_request ;
-exports.createEntry = handle_POST_request ;
+//  export the API methods
+exports.fetchEntries = return_all_posts ;
+exports.createEntry = add_blog_post  ;
